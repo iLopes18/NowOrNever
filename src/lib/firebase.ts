4662baa39c11e847947ab4b1ad8a3b1272bd1376
iLopes18 <18isaaclopes@gmail.com>
2026-05-17 18:15:12 +0100

    Change firebase config import from .json to .js

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from '../../firebase-applet-config.json';
+import firebaseConfig from '../../firebase-applet-config.js';
 
 const app = initializeApp(firebaseConfig);
 // CRITICAL: The app will break without specifying the firestoreDatabaseId
